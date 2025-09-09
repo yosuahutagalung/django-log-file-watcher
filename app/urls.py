f03a4fe93f2e55fa0fb3b4abbaf78f43259bef10
Yosua Hutagalung <yosua.hutagalung05@gmail.com>
2025-09-09 11:19:19 +0700

    Create base log detail page

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('log/<int:log_id>', views.LogDetailView.as_view(), name='log_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from app.models import LogFile
 
 
-# Create your views here.
 class IndexView(LoginRequiredMixin, ListView):
     template_name = 'app/home.html'
     model = LogFile
+
+
+class LogDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'app/log-details.html'
+    model = LogFile
+    pk_url_kwarg = 'log_id'
+

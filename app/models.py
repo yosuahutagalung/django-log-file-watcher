2591677f51005795ebf6d92474e730cd6907ddf1
Yosua Hutagalung <yosua.hutagalung05@gmail.com>
2025-09-09 09:55:58 +0700

    Create LogFile model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class LogFile(TimestampBaseModel):
+    name = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+    encoding = models.CharField(max_length=20, default='utf-8')
+
+    class Meta:
+        db_table = 'log_files'
+
+    

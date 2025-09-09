1542de3f199decc73a1a4663e26545bd9bd15627
Yosua Hutagalung <yosua.hutagalung05@gmail.com>
2025-09-09 14:59:05 +0700

    Implement log box in log page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import ListView, DetailView
 
+from app.helpers import tail
 from app.models import LogFile
 
 
@@ -14,4 +15,17 @@ class LogDetailView(LoginRequiredMixin, DetailView):
     template_name = 'app/log-details.html'
     model = LogFile
     pk_url_kwarg = 'log_id'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        lines = []
+        try:
+            lines = tail(self.object.path, 500)
+        except FileNotFoundError:
+            messages.error(self.request, 'Log file not found')
+        except Exception as e:
+            messages.error(self.request, f'Error: {e}')
+
+        ctx['lines'] = lines
+        return ctx
 

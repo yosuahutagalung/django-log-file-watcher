962196511b96b088a13aec734eb6105ce0afae45
Yosua Hutagalung <yosua.hutagalung05@gmail.com>
2025-09-09 16:49:41 +0700

    Adjust run_server script

--- run_server.py
+++ run_server.py
@@ -4,9 +4,9 @@ from decouple import config
 if __name__ == "__main__":
     config = uvicorn.Config(
         'core.asgi:application',
-        host=config('SERVER_HOST', default='127.0.0.1'),
-        port=config('SERVER_PORT', default='8000'),
-        workers=1,
+        host=config('SERVER_HOST', cast=str, default='127.0.0.1'),
+        port=config('SERVER_PORT', cast=int, default='8000'),
+        workers=1,  # Due to Channels currently uses in-memory channels layer, worker count is limited to only one.
         log_level='info',
         reload=False
     )
